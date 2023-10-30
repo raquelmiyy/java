d608250617698608efce626fd290a2e698c592b3
Raquel Guimarães <126600346+raquelmiyy@users.noreply.github.com>
2023-10-30 12:07:40 -0300

    pokemon

--- Pokemon.java
+++ Pokemon.java
@@ -1,51 +1,58 @@
+package resolucao.pokemon;
+
 public class Pokemon {
-        private String nome;
-        private String tipo;
-        private Integer forca;
-        private Integer doces;
 
-        public Pokemon(String nome, String tipo, Integer forca) {
-            this.nome = nome;
-            this.tipo = tipo;
-            this.forca = forca;
-            this.doces = 0;
-        }
+    private String nome;
+    private String tipo;
+    private Double forca;
+    private Integer doces;
 
-        public String getNome() {
-            return nome;
-        }
+    public Pokemon(String nome, String tipo, Double forca) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.forca = forca;
+        this.doces = 0;
+    }
 
-        public String getTipo() {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
         return tipo;
-        }
-        public Integer getForca() {
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getForca() {
         return forca;
-        }
+    }
 
-       public Integer getDoces() {
+    public void setForca(Double forca) {
+        this.forca = forca;
+    }
+
+    public Integer getDoces() {
         return doces;
-       }
+    }
 
+    public void setDoces(Integer doces) {
+        this.doces = doces;
+    }
 
-        public void ganharDoces(Integer qtd) {
-            doces += qtd;
-        }
-
-        public Boolean podeEvoluir() {
-            return doces >= 50;
-        }
-
-        public void evoluir(String nomeEvolucao) {
-            if (podeEvoluir()) {
-                nome = nomeEvolucao;
-                doces -= 50;
-                System.out.println("Pokémon " + nomeEvolucao + " evoluiu para: " + nome);
-            } else {
-                System.out.println("Não foi possível realizar a evolução.");
-            }
-        }
-
-    public void aumentarForca(Integer ignoredI) {
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "nome='" + nome + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", forca=" + forca +
+                ", doces=" + doces +
+                '}';
     }
 }
-
--- TreinadorPokemon.java
+++ TreinadorPokemon.java
@@ -1,24 +1,55 @@
+package resolucao.pokemon;
+
 public class TreinadorPokemon {
-        private String nome;
-        private Integer nivel;
 
-        public TreinadorPokemon(String nome) {
-            this.nome = nome;
-            this.nivel = 1;
+    private String nome;
+
+    private Integer nivel;
+
+    public TreinadorPokemon(String nome) {
+        this.nome = nome;
+        this.nivel = 0;
+    }
+
+    public void treinarPokemon(Pokemon pokemon) {
+        pokemon.setForca(pokemon.getForca() * 1.1);
+        pokemon.setDoces(pokemon.getDoces() + 10);
+        nivel += 2;
+    }
+
+    public void evoluirPokemon(Pokemon pokemon, String nomeEvolucao) {
+        if (pokemon.getDoces() >= 50) {
+            String antigaEvolucao = pokemon.getNome();
+            pokemon.setNome(nomeEvolucao);
+            pokemon.setDoces(pokemon.getDoces() - 50);
+            nivel += 10;
+            System.out.println(String.format("Pokémon %s evoluiu para -> %s", antigaEvolucao, nomeEvolucao));
+        } else {
+            System.out.println("Não foi possível realizar operação");
         }
+    }
 
-        public String getNome() {
+    public String getNome() {
         return nome;
-        }
+    }
 
-        public Integer getNivel() {
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNivel() {
         return nivel;
-        }
+    }
 
-        public void treinarPokemon(Pokemon pokemon) {
-            pokemon.ganharDoces(10);
-            pokemon.aumentarForca(10);
-            nivel += 2;
-        }
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
 
+    @Override
+    public String toString() {
+        return "TreinadorPokemon{" +
+                "nome='" + nome + '\'' +
+                ", nivel=" + nivel +
+                '}';
+    }
 }
